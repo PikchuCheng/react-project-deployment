1381855300d56cb41e48515ceb86d7e900890fbe
PikchuCheng <pikchucheng@gmail.com>
2023-10-31 15:41:11 +0100

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -11,29 +11,27 @@ export default function Navbar() {
   return (
     <div className= "navbar">
 
+        <a href="#!">FoodiesParadise</a>
 
-
-        <a href="#!">FoodiesHub</a>
-
-
+        <div className='nav-links'>
 
         <a href="#!">Home</a>
         <a href="#!">Recipes</a>
         <a href="#!">Settings</a>
         
-        
-         
-        Navbar
-
-
-        <div>
-
-
-
-
         </div>
         
-        
+      <div onClick={()=> setShowSidebar(!showSidebar)} className={showSidebar ? "sidebar-icon active" : "sidebar-icon"} >
+
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+
+
+
+
+      </div>
+
         
         </div>
   )
