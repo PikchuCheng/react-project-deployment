23c83b06e7c066c41cf6e5f812d42f551716470c
PikchuCheng <pikchucheng@gmail.com>
2023-11-03 00:41:57 +0100

    footer update

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
-
 import './style/index.scss'
 import Recipes from './pages/Recipes'
 import Navbar from './Components/Navbar'
 import Hero from './Components/Hero'
+import Footer from './Components/Footer'
+
+
+// import {BrowserRouter, Router, Routes} from "react-router-dom"
+// import Settings from ".pages/Settings"
 
 function App() {
  
   return (
     <>
-     
-    <h3>this is a final project</h3> 
+      
 
     <Navbar />
+
     <Hero/>
     <Recipes/>
 
     <h4>By Gabriella Esther Pikchu 2023</h4>
-  
+    <Footer />
+
+
     </>
   )
 }
