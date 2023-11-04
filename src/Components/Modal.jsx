a823b2bf3e721b2738b0a50bb520db267d29f355
esther Mwangi-Häber <esther.wangechi.mwangi@gmail.com>
2023-11-04 22:36:44 +0100

    changes

---
+++ src/Components/Modal.jsx
@@ -0,0 +1,32 @@
+// import { useMyContext } from './MyContext'; 
+// import { MdCancel } from "react-icons/md";
+
+
+
+
+// function Modal() {
+//   const { selectedItem, setIsModalOpen, isModalOpen } = useMyContext();
+
+//   const handleCancelModal = () => {
+//     setIsModalOpen(!isModalOpen);
+//   };
+
+
+//   return (
+// <> 
+// {selectedItem && isModalOpen ? (
+//         <div className='modalcontainer'>
+//           <MdCancel className='btn-cancel' onClick={handleCancelModal} />
+//           <h2 className='titlecard'>{selectedItem.title}</h2>
+//           <img src={selectedItem?.image} alt="" />
+          
+//           <p></p>
+//           <h3>Preparation Time: {selectedItem.readyInMinutes}min</h3>
+//           <h3>Potions: {selectedItem.servings}</h3>
+          
+//         </div>
+//       ) : null}
+//     </>
+//   );
+// }
+// export default Modal
