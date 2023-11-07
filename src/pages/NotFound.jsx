361bf766738850df6ecedb9c653aad0af4323811
Gabriella <icaro7.78@gmail.com>
2023-11-07 19:21:17 +0100

    Finish Version for Presentention

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,14 +3,14 @@ function NotFound() {
   return (
     <div className="body-not-found">
       <div className="page">
-        <h1>Error 404</h1>
-        <h2>PAGE NOT FOUND</h2>
+        <h1 className="moire-title">Error 404</h1>
+        <h2 className="moire-title" >PAGE NOT FOUND</h2>
     </div>
     <div className="page1">
-        <h1>GO</h1>
+        <h1 className="moire-title">GO</h1>
     </div>
     <div className="pag">
-        <h1>HOME</h1>
+        <h1 className="moire-title">HOME</h1>
     </div>
     <div className="moire"></div>    
     <div className="moire2"></div>
